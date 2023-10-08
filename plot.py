bd440b73e55dfac2254ab0aac914e125bf12a47f
Kristóf Bácskai <45177246+Kris030@users.noreply.github.com>
2023-10-08 11:29:28 +0200

    Finally finish watching on plot.py

--- plot.py
+++ plot.py
@@ -1,19 +1,13 @@
 #!/bin/env python3
 from matplotlib.widgets import Slider
 import matplotlib.pyplot as plt
-from watchfiles import watch
 import numpy as np
 import argparse
 import wave
 import sys
 
-parser = argparse.ArgumentParser(description='Plot da wave')
-parser.add_argument('--input_file', '-i', default='test.wav', help='the wav file ya retard')
-args = parser.parse_args()
-
-def f():
-	print("f")
-	spf = wave.open(args.input_file, 'r')
+def get_data(file):
+	spf = wave.open(file, 'r')
 
 	# Extract Raw Audio from Wav File
 	signal = spf.readframes(-1)
@@ -25,60 +19,79 @@ def f():
 		sys.exit(0)
 
 	return signal / 0x7FFF
-to_plot = f()
 
-fig, ax = plt.subplots()
+def main():
+	parser = argparse.ArgumentParser(description='Plot da wave')
+	parser.add_argument('--input_file', '-i', default='test.wav', help='the wav file ya retard')
+	parser.add_argument('--watch', '-w', action='store_true', help='watch da wav 4 changes')
+	args = parser.parse_args()
 
-ax.set_title('Wave')
-ax.set_xlabel('Sample')
-ax.set_ylabel('Amplitude')
+	to_plot = get_data(args.input_file)
 
-plt.subplots_adjust(bottom=0.25)
+	fig, ax = plt.subplots()
 
-plt.plot(to_plot)
+	ax.set_title('Wave')
+	ax.set_xlabel('Sample')
+	ax.set_ylabel('Amplitude')
 
-spos = Slider(
-	plt.axes([0.1, 0.05, 0.8, 0.03], facecolor='yellow'),
-	'Pos',
-	0.0, 1.0,
-	valinit=0
-)
+	plt.subplots_adjust(bottom=0.25)
 
-zoom = Slider(
-	plt.axes([0.1, 0.1, 0.8, 0.03], facecolor='yellow'),
-	'Zoom',
-	0.0, 1.0,
-	valinit=0.995
-)
+	ax.plot(to_plot)
 
-def update(_):
-	p = len(to_plot) * spos.val
-	z = len(to_plot) * (1 - zoom.val) / 2
-	ax.axis([
-		p - z,
-		p + z,
-		-1, 1,
-	])
-	fig.canvas.draw_idle()
+	spos = Slider(
+		plt.axes([0.1, 0.05, 0.8, 0.03], facecolor='yellow'),
+		'Pos',
+		0.0, 1.0,
+		valinit=0
+	)
 
-update(None)
+	zoom = Slider(
+		plt.axes([0.1, 0.1, 0.8, 0.03], facecolor='yellow'),
+		'Zoom',
+		0.0, 1.0,
+		valinit=0.995
+	)
 
-spos.on_changed(update)
-zoom.on_changed(update)
+	def update(_):
+		p = len(to_plot) * spos.val
+		z = len(to_plot) * (1 - zoom.val) / 2
+		ax.axis([
+			p - z,
+			p + z,
+			-1, 1,
+		])
+		fig.canvas.draw_idle()
 
-fig.canvas.manager.set_window_title('Wave')
-plt.show(block=False)
+	update(None)
 
-for changes in watch(args.input_file, rust_timeout=200, yield_on_timeout=True):
-	if not plt.fignum_exists(1):
-		break
+	spos.on_changed(update)
+	zoom.on_changed(update)
 
-	# plt.pause(0.1)
-	if len(changes) != 0:
-		to_plot = f()
+	fig.canvas.manager.set_window_title('Wave')
+	plt.show(block=not args.watch)
 
-		fig.clear()
-		plt.plot(to_plot)
+	def watch():
+		from watchfiles import watch as w
 
-	fig.canvas.draw_idle()
-	fig.canvas.start_event_loop(0.3)
+		for changes in w(args.input_file, rust_timeout=200, yield_on_timeout=True):
+			if not plt.fignum_exists(1):
+				break
+
+			if len(changes) != 0:
+				to_plot = get_data(args.input_file)
+				ax.clear()
+				ax.plot(to_plot)
+				update(None)
+			else:
+				fig.canvas.draw_idle()
+
+			fig.canvas.start_event_loop(0.3)
+
+	if args.watch:
+		watch()
+
+if __name__ == '__main__':
+	try:
+		main()
+	except KeyboardInterrupt:
+		pass
